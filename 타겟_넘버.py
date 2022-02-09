66d2b1df72a2c96ce0863bb8b8b71b64906ff89b
dawonseo <oloielo@naver.com>
2022-02-09 17:46:33 +0900

    Add 6주차

---
+++ 타겟_넘버.py
@@ -0,0 +1,17 @@
+# 프로그래머스 타겟 넘버
+
+def solution(numbers, target):
+
+    s_list = [0]
+    n = 0
+    lim = 0
+    i = 0
+    for num in numbers:
+        while i <= lim:
+            s_list.append(s_list[i] + num)
+            s_list.append(s_list[i] - num)
+            i += 1
+        n += 1
+        lim += 2**n
+
+    return s_list[-2**len(numbers):].count(target)
