1452c76fcdd4b1ce61f63b30e197036a2027d50e
Dawon Seo <85838334+dawonseo@users.noreply.github.com>
2022-02-24 18:21:00 +0900

    Update 전화번호_목록.py

--- 전화번호_목록.py
+++ 전화번호_목록.py
@@ -1,7 +1,6 @@
 def solution(phone_book):
-    ans = True
     phone_book.sort()
     for i in range(len(phone_book)-1):
         if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
             return False
-    return ans
+    return True
