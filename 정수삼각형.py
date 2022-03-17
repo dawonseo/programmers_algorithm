655928416225e3078a1b2788a354fb1439f7a6a2
Dawon Seo <85838334+dawonseo@users.noreply.github.com>
2022-03-17 16:22:35 +0900

    Update 정수삼각형.py

--- 정수삼각형.py
+++ 정수삼각형.py
@@ -1,11 +1,11 @@
 def solution(triangle):
     for i in range(1, len(triangle)):
         for j in range(len(triangle[i])):
-            if j == 0:
+            if j == 0:                                      # 왼쪽 끝일 경우
                 triangle[i][j] += triangle[i - 1][j]
-            elif j == len(triangle[i]) - 1:
+            elif j == len(triangle[i]) - 1:                 # 오른쪽 끝일 경우
                 triangle[i][j] += triangle[i - 1][j - 1]
-            else:
+            else:                                           # 중간일 경우
                 triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
 
     return max(triangle[-1])
