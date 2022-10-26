3386417a7ace0f05335d149f1cf82bdbf7aa9f1e
Dawon Seo <85838334+dawonseo@users.noreply.github.com>
2022-10-27 00:49:13 +0900

    Update 조이스틱.py

--- 조이스틱.py
+++ 조이스틱.py
@@ -1,13 +1,36 @@
 def solution(name):
-    cursor = 0
+    if set(name) == {"A"}:
+        return 0
     ans = 0
+    prev = ''
+    n = len(name)
+    s = 0
+    e = n - 1
+    while s < n:
+        if name[s] != 'A':
+            break
+        else:
+            s += 1
+    while e >= 0:
+        if name[e] != 'A':
+            break
+        else:
+            e -= 1
+    m = min(e, n - s)
     
-    while cursor < len(name):
-        this_word = ord(name[cursor])
+    for i in range(s, e+1):
+        this = name[i]
+        this_word = ord(this)
         ans += min(91 - this_word, this_word - 65)
-        cursor += 1
-    
-    ans += cursor - 1
-    if name[1] == 'A': ans -= 1
-    
-    return ans
+        if prev == 'A':
+            if this == 'A':
+                continue
+            else:
+                end = i-1
+                m = min(m, (start - 1) + (2 * ((n - end) - 1)), 2 * (start - 1) + ((n - end) - 1))
+        else:
+            if this == 'A':
+                start = i
+        prev = this
+
+    return ans + m
