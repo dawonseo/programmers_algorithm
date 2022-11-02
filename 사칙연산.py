e0348e92d7b20886274944b8afeccbcadb4a55f3
Dawon Seo <85838334+dawonseo@users.noreply.github.com>
2022-11-02 23:33:59 +0900

    Update 사칙연산

--- 사칙연산.py
+++ 사칙연산.py
@@ -3,10 +3,20 @@ def solution(arr):
     MIN_DP = [[INF for _ in range(len(arr))] for _ in range(len(arr))]
     MAX_DP = [[-INF for _ in range(len(arr))] for _ in range(len(arr))]
     
-    for step in range(0, len(arr)-1, 2):
-        for i in range(len(arr)-1):
+    for i in range(0, len(arr), 2):
+        MIN_DP[i][i] = int(arr[i])
+        MAX_DP[i][i] = int(arr[i])
+    
+    for step in range(2, len(arr), 2):
+        for i in range(0, len(arr)-step, 2):
             j = i + step
+            for k in range(i+1, j, 2):
+                if arr[k] == '+':
+                    MIN_DP[i][j] = min(MIN_DP[i][j], MIN_DP[i][k-1] + MIN_DP[k+1][j])
+                    MAX_DP[i][j] = max(MAX_DP[i][j], MAX_DP[i][k-1] + MAX_DP[k+1][j])
+                elif arr[k] == '-':
+                    MIN_DP[i][j] = min(MIN_DP[i][j], MIN_DP[i][k-1] - MAX_DP[k+1][j])
+                    MAX_DP[i][j] = max(MAX_DP[i][j], MAX_DP[i][k-1] - MIN_DP[k+1][j])
+        
+    return MAX_DP[0][-1]
             
-            if i == j:
-                MIN_DP[i][j] = arr[i]
-                MAX_DP[i][j] = arr[i]
